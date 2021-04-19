ef17c68dd1bbd4feed8de15ca79ca06f01cea158
MIchael Ekornrud <s341866@oslomet.no>
2021-04-19 10:26:59 +0200

    Initial commit

---
+++ src/main/java/RestServer/RestServerApp.java
@@ -0,0 +1,17 @@
+package RestServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServerApp {
+
+    //Main methos for application
+    //localhost:8080/path
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestServerApp.class, args);
+    }
+
+
+}
